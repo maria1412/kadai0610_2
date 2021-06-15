e20d302d1061b301eb96f4e1207848e7dc467c5c
maria1412 <82562294+maria1412@users.noreply.github.com>
2021-06-15 17:59:36 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("a")
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("b")
+    basic.pause(100)
+    basic.clearScreen()
+})
+let 文字列の配列 = ["a", "b"]
